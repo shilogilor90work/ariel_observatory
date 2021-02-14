3d0a323c7a3174f3d05dab303686f193ec8d8b02
shilogilor90work <shilogilor90work@gmail.com>
2021-02-14 14:52:52 +0200

    post instead of get

--- scraping_IMS.py
+++ scraping_IMS.py
@@ -1,8 +1,8 @@
 from IMS_config import current_forcast_site, zone_ariel, future_forcast_site
 import requests
 
-x = requests.get(current_forcast_site)
+x = requests.post(current_forcast_site)
 print(x.json()['data'][zone_ariel])
 print()
-y = requests.get(future_forcast_site+zone_ariel)
+y = requests.post(future_forcast_site+zone_ariel)
 print(y.json())
