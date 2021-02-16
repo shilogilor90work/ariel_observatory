7092a76e76a6d455338e05c4d8580f6ca52c8b65
shilogilor90work <shilogilor90work@gmail.com>
2021-02-16 12:43:05 +0200

    future forcast

---
+++ IMS/weekly_forcast/scraping_IMS.py
@@ -0,0 +1,54 @@
+from IMS_weekly_config import current_forcast_site, zone_ariel, future_forcast_site
+import json
+import requests
+import mysql.connector
+
+# URL
+url = requests.post(future_forcast_site + zone_ariel)
+
+
+# request
+response = requests.request("POST", url)
+# dump data
+data= json.loads(response.text.encode('utf8'))
+# parse data
+current_data = data['data'][0]['channels']
+temp_day = [day for day in data['data']]
+current_data = {}
+for day in data['data']:
+    for hour in day:
+        for specific_time in hour:
+            current_data[specific_time[forecast_time]] = {"forecast_time": specific_time["forecast_time"], "weather_code": specific_time["weather_code"], "temperature": specific_time["temperature"], "weather": ""}
+            try:
+                current_data[specific_time[forecast_time]]["weather"] = whether[specific_time["weather_code"]]
+
+# print, will be converted to send data to DB
+print(current_data)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="shilo",
+  database="IMS"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE weekly_weather (forecast_time VARCHAR(255) , PRIMARY KEY(`forecast_time`) , weather_code VARCHAR(255) , weather VARCHAR(255) , temperature VARCHAR(255)")
+sql = "INSERT INTO weekly_weather (forecast_time, weather_code, weather, temperature) VALUES (%s, %s, %s, %s)"
+
+for single_current_data in current_data:
+    val = (str(single_current_data['forecast_time']), str(single_current_data['weather_code']), str(single_current_data['weather']), str(single_current_data['temperature']))
+    mycursor.execute(sql, val)
+
+mydb.commit()
+
+
+# sites to pull data incase API TOKEN not allowed
+#
+# x = requests.post(current_forcast_site)
+# print(x.json()['data'][zone_ariel])
+# print()
+# y = requests.post(future_forcast_site+zone_ariel)
+# print(y.json())
+#
