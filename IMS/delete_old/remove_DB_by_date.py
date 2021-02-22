24151802a0ee1ea86a76258e65822be6c2a24893
shilogilor90work <shilogilor90work@gmail.com>
2021-02-22 10:42:54 +0200

    trial 16

--- IMS/delete_old/remove_DB_by_date.py
+++ IMS/delete_old/remove_DB_by_date.py
@@ -1,11 +1,5 @@
 from remove_config import DAYS_COUNT_BACK, table_names
 import mysql.connector
-# from datetime import datetime, timedelta
-
-# N = DAYS_COUNT_BACK
-
-# date_N_days_ago = datetime.now() - timedelta(days=N)
-
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -16,13 +10,5 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 for table, column in table_names.items():
     string = "delete from " + table + " where STR_TO_DATE(LEFT(" + column + " , 10), '%Y-%m-%d') > CURDATE() - INTERVAL " + str(DAYS_COUNT_BACK) + " DAY"
-    # + date_N_days_ago.strftime('%Y-%m-%d') + " > " + column
-    mycursor.execute(string
-        # """
-        #     delete
-        #     from %s p1
-        #     where STRCMP(%s, p1.%s) == 1
-        # """,
-        # (table, date_N_days_ago.strftime('%Y-%m-%d'), column,)
-    )
+    mycursor.execute(string)
 mydb.commit()
