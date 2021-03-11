4a50f3961528db7d0c69bf21f2198bdaa70cb78f
shilogilor <shilogilor1990work@gmail.com>
2021-03-11 11:38:01 +0200

    imports

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from forcast import views
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, namespace='dashboard'),
+    path('',  include(('forcast.urls', 'dashboard'), namespace='dashboard'),
 ]
