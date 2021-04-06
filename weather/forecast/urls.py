c65d097242efd2d5ab6e09f88ea87e9c0337879a
shilogilor <shilogilor1990work@gmail.com>
2021-04-06 13:41:20 +0300

    slash

--- weather/forecast/urls.py
+++ weather/forecast/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',  views.status_view, name="dashboard"),
-    path('rules',  views.rules_view, name="rules"),
+    path('rules/',  views.rules_view, name="rules"),
     path('rules/hello_world',  views.hello_world, name="hello_world"),
 ]
