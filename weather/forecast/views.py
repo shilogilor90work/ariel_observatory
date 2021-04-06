31888ff8af8ebea956b53c4a3dfcbaf06af9b305
shilogilor <shilogilor1990work@gmail.com>
2021-04-06 13:26:42 +0300

    response

--- weather/forecast/views.py
+++ weather/forecast/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from forecast.models import Weekly, Current_Weather, Rules
 
