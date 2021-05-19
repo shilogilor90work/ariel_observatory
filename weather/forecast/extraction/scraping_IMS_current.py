69c2635d717b334e39172bff36a4f74eb926d8c3
shilogilor90work <shilogilor1990work@gmail.com>
2021-05-19 13:48:05 +0300

    more fixes

--- weather/forecast/extraction/scraping_IMS_current.py
+++ weather/forecast/extraction/scraping_IMS_current.py
@@ -28,9 +28,9 @@ def scrape_IMS_current():
     if current_item.exists():
         current_item.objects.update(rain=str(name_value['Rain']), wsmax=str(name_value['WSmax']), wdmax=str(name_value['WDmax']), ws=str(name_value['WS']),
         wd=str(name_value['WD']), stdwd=str(name_value['STDwd']), td=str(name_value['TD']), tw=str(name_value['TW']), tdmax=str(name_value['TDmax']),
-        tdmin=str(name_value['TDmin']), ws1mm=str(name_value['WS1mm']), ws10mm=str(name_value['WS10mm']), time=str(name_value['time']), tg=str(name_value['TG']), rh=str(name_value['RH']))
+        tdmin=str(name_value['TDmin']), ws1mm=str(name_value['WS1mm']), ws10mm=str(name_value['Ws10mm']), time=str(name_value['Time']), tg=str(name_value['TG']), rh=str(name_value['RH']))
     else:
         Current_Weather.objects.create(current_time=current_time, rain=str(name_value['Rain']), wsmax=str(name_value['WSmax']), wdmax=str(name_value['WDmax']), ws=str(name_value['WS']),
         wd=str(name_value['WD']), stdwd=str(name_value['STDwd']), td=str(name_value['TD']), tw=str(name_value['TW']), tdmax=str(name_value['TDmax']),
-        tdmin=str(name_value['TDmin']), ws1mm=str(name_value['WS1mm']), ws10mm=str(name_value['WS10mm']), time=str(name_value['time']), tg=str(name_value['TG']), rh=str(name_value['RH']))
+        tdmin=str(name_value['TDmin']), ws1mm=str(name_value['WS1mm']), ws10mm=str(name_value['Ws10mm']), time=str(name_value['Time']), tg=str(name_value['TG']), rh=str(name_value['RH']))
     print(name_value)
