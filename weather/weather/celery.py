1b1e5072958e63354c612840344370b3d5ce2022
amiellib <amiel222000@gmail.com>
2021-07-27 17:21:53 +0300

    name

--- weather/weather/celery.py
+++ weather/weather/celery.py
@@ -4,9 +4,9 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ariel_observatory.settings')
 
-app = Celery('weather')
+app = Celery('ariel_observatory')
 
 app.comfig_from_object('django.conf::settings' ,namespace='CELERY')
 
