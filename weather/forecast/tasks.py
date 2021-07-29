3fa70339ab4117c631ae2dc41ed0c25530418f1d
shilogilor90work <shilogilor1990work@gmail.com>
2021-07-29 20:13:04 +0300

    comments

--- weather/forecast/tasks.py
+++ weather/forecast/tasks.py
@@ -8,9 +8,13 @@ from .extraction.scraping_IMS_weekly import scrape_IMS_weekly
 
 @periodic_task(run_every=crontab(minute='0,10,20,30,40,50'))
 def current_scrape():
+    print("starting current scrape")
     scrape_IMS_current()
+    print("finished current scrape")
 
 
 @periodic_task(run_every=crontab(minute=0, hour='6,18'))
 def weekly_scrape():
+    print("starting weekly scrape")
     scrape_IMS_weekly()
+    print("finished weekly scrape")
