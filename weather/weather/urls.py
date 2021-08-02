28b42f5d7e98841034080c4972133120734d2838
amiellib <amiel222000@gmail.com>
2021-08-02 11:08:06 +0300

    log

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', include('django.contrib.auth.urls')),
     path('', include('forecast.urls')),
 ]
