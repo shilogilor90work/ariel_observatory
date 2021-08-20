1ece24e60dc69a4fd71915e43870220f5aa7dfeb
amiellib <amiel222000@gmail.com>
2021-08-20 11:58:32 +0300

    times

--- weather/forecast/tasks.py
+++ weather/forecast/tasks.py
@@ -4,6 +4,8 @@ from celery.decorators import periodic_task
 from celery.schedules import crontab
 from .extraction.scraping_IMS_current import scrape_IMS_current
 from .extraction.scraping_IMS_weekly import scrape_IMS_weekly
+from .extraction.remove_DB_by_date import delete_old()
+
 
 
 @periodic_task(run_every=crontab(minute='0,10,20,30,40,50'))
@@ -18,3 +20,9 @@ def weekly_scrape():
     print("starting weekly scrape")
     scrape_IMS_weekly()
     print("finished weekly scrape")
+
+@periodic_task(run_every=crontab(minute='0,30'))
+def delete_old():
+    print("starting delete old DB")
+    delete_old()
+    print("finished delete old DB")
