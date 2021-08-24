9a99bb01efee1c0fbb6557769ddb5fbf7abf88a2
amiellib <amiel222000@gmail.com>
2021-08-24 20:46:16 +0300

    C#_ACP_example

---
+++ Amiel/Observatory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amiel
+{
+    public class Observatory
+    {
+        public Observatory()
+        {
+            bool safe_or_not = false;
+        }
+
+        public bool safe()
+        {
+            //url of api_status 
+            var url = "http://18.222.50.25/api_status/ariel";
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpRequest.Accept = "application/json";
+
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                //if status is safe or not
+
+                
+                if (result.IndexOf("green") != -1)
+                {
+                    // status is safe to operite
+                    safe_or_not = true;
+                }
+                else
+                {
+                    // status is not safe to operite
+                    safe_or_not = false;
+                }
+            }
+            //return the status for ACP.weather
+            return safe_or_not;
+
+        }
+    }
+}
