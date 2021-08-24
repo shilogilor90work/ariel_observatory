40c8e9a9a695926e774a92da4274f98169561411
shilogilor90work <shilogilor1990work@gmail.com>
2021-08-24 20:41:56 +0300

    /ariel

--- weather/forecast/urls.py
+++ weather/forecast/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('',  views.status_view, name="dashboard"),
-    path('api_status',  views.api_status, name="api_status"),
-    path('api_current',  views.api_current, name="api_current"),
+    path('api_status/ariel',  views.api_status, name="api_status"),
+    path('api_current/ariel',  views.api_current, name="api_current"),
     path('rules/',  views.rules_view, name="rules"),
     path('rules/hello_world/',  views.hello_world, name="hello_world"),
     path('rules/update_rules/',  views.update_rules, name="hello_world"),
