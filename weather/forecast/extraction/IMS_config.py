3858fdcbe42f6250fe52850cc637822c03f0d728
shilogilor90work <shilogilor1990work@gmail.com>
2021-08-29 19:45:35 +0300

    new zone allow null

--- weather/forecast/extraction/IMS_config.py
+++ weather/forecast/extraction/IMS_config.py
@@ -48,5 +48,5 @@ channel = {
 API_TOKEN = "f058958a-d8bd-47cc-95d7-7ecf98610e47"
 current_forecast_site = "https://ims.gov.il/he/now_analysis"
 #lid 73
-zone_ariel = "73"
+zone_ariel = "21"
 future_forecast_site = "https://ims.gov.il/he/forecast_data/"
